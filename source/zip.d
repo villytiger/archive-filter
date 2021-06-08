8ad926bcf70893ef9bfb3dd859802d6ba81de723
Михалицын Петр <pmikhalicin@rutoken.ru>
2021-06-08 16:20:58 +0300

    fix some warnings

--- source/zip.d
+++ source/zip.d
@@ -462,7 +462,7 @@ public:
                 if (count < mHeader.entriesCountOnThisDisk.max) {
                         mHeader.entriesCountOnThisDisk = count.to!ushort().toLittleEndian();
                 } else {
-                        mHeader.entriesCountOnThisDisk = -1.to!ushort;
+                        mHeader.entriesCountOnThisDisk = mHeader.entriesCountOnThisDisk.max;
                 }
         }
 
@@ -470,7 +470,7 @@ public:
                 if (count < mHeader.entriesCount.max) {
                         mHeader.entriesCount = count.to!ushort().toLittleEndian();
                 } else {
-                        mHeader.entriesCount = -1.to!ushort;
+                        mHeader.entriesCount = mHeader.entriesCountOnThisDisk.max;
                 }
         }
 
